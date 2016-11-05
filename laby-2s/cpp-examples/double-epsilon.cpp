94d709ba361bb92a919e1d976c0194bee8111269
nickkolok <nickkolok@mail.ru>
2016-11-05 22:26:07 +0300

    Листинг с хитрым округлением в макросе

---
+++ laby-2s/cpp-examples/double-epsilon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define EPSILON 1/1000/1000/1000
+
+using namespace std;
+
+int main() {
+	cout <<          EPSILON  << endl;
+	cout << (double) EPSILON  << endl;
+	cout << (double)(EPSILON) << endl;
+	return 0;
+}
+// Что будет выведено на экран и почему?

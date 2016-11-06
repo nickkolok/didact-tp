927797e4af2a13d02c03e52598275c54b3d1a1d0
nickkolok <nickkolok@mail.ru>
2016-11-06 15:34:40 +0300

    Типовая ошибка: повторяющиеся вычисления в цикле

---
+++ laby-2s/typical-errors-snippets/duplicate-calculations-for-1-right.cpp
@@ -0,0 +1,6 @@
+int len = strlen(str);
+for (int i = 0; i < len; i++) {
+	if (str[i] == '!') {
+		exMarksCounter++;
+	}
+}
---
+++ laby-2s/typical-errors-snippets/duplicate-calculations-for-2-right.cpp
@@ -0,0 +1,5 @@
+for (int i = 0; str[i]; i++) {
+	if (str[i] == '!') {
+		exMarksCounter++;
+	}
+}

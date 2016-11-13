07dfaecffaa6e56c6cc7d37d7fb615ebee1a69ef
nickkolok <nickkolok@mail.ru>
2016-11-13 23:06:24 +0300

    Типовая ошибка: поиск минимума с нуля

---
+++ laby-2s/typical-errors-snippets/for-from-0-instead-of-1-1-right.cpp
@@ -0,0 +1,6 @@
+int min = mas[0];
+for(int i = 1; i < size; i++){
+	if(mas[i] < min){
+		min = mas[i];
+	}
+}
---
+++ laby-2s/typical-errors-snippets/for-from-0-instead-of-1-1-wrong.cpp
@@ -0,0 +1,6 @@
+int min = mas[0];
+for(int i = 0; i < size; i++){
+	if(mas[i] < min){
+		min = mas[i];
+	}
+}

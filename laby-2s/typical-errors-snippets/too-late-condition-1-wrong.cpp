4fabe6c214c2031e7c98f78c861c9a3fc0e18a77
nickkolok <nickkolok@mail.ru>
2016-11-13 22:38:19 +0300

    Типовая ошибка: несвоевременно поздняя проверка входных данных

---
+++ laby-2s/typical-errors-snippets/too-late-condition-1-wrong.cpp
@@ -0,0 +1,18 @@
+char* deleteDots(char* str) {
+	unsigned int len = strlen(str);
+	char* newStr = new char[len + 1];
+	unsigned int i = 0, z = 0;
+
+	if (!len)
+		return NULL;
+
+	while (i < len){
+		if(str[i]!='.'){
+			newStr[z] = str[i];
+			z++;
+		}
+		i++;
+	}
+	return newStr;
+}
+

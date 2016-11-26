983d0ca16713b2e6b549e41b7a761698ddfcb425
Николай Авдеев <nickkolok@mail.ru>
2016-11-26 14:36:39 +0300

    Разбивка таблицы аналогий на 2 страницы + case

---
+++ laby-2s/code-snippets/analog-case.cpp
@@ -0,0 +1,12 @@
+switch(n) {
+	case 1:
+		cout << 1;
+		cout << " (один)";
+	break;
+	case 2:
+		cout << 2;
+		cout << " (два)";
+	break;
+	default:
+		cout << "Не 1 и не 2";
+}
---
+++ laby-2s/code-snippets/analog-case.pas
@@ -0,0 +1,13 @@
+case n of
+	1: begin
+		write(1);
+		write(' (один)');
+	end;
+	2: begin
+		write(2);
+		write(' (два)');
+	end;
+	else
+		write('Не 1 и не 2');
+end;
+

d9502eb26d653c8fe2e8b5e695f99fb04070d788
Николай Авдеев <nickkolok@mail.ru>
2017-03-21 20:08:51 +0300

    Собственно упражнения

---
+++ exercises-listings/TO-if-duplicate-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	int s=0;
+	int i=0;
+	int j=0; 
+	char str[100],newstr[100];
+	char *y,*m;
+	y = str;
+	m = newstr;
+	cout << "Введите строку:" << endl;
+	gets(str);
+	cout << "Исходная строка:";
+	cout << str << endl;
+	do{
+ 		if(*(y+i) == *(y+i+1)){
+			*(m+j) = 'z';
+			j++;
+			i += 2;
+			s++;
+		} else {
+			*(m+j) = *(y+i);
+			j++;
+			i++;
+		}
+	}while(*(y+i)); 
+
+	*(m+j) = 0;
+
+	cout << "Полученная строка " << newstr << endl;
+	cout << "Количество пар одинаковых сиволов=" << s << endl;
+
+	return 0;
+}
